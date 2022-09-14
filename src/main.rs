f95f70f3edbba7057d1b32070e11a20bb5d208ec
Neosb <museyoucoulduse@gmail.com>
2022-09-14 03:33:51 +0200

    working test

--- src/main.rs
+++ src/main.rs
@@ -16,3 +16,14 @@ fn main() {
     #[cfg(feature = "async")]
     futures_lite::future::block_on(p.interact()).expect("Failed to start interact");
 }
+
+mod tests {
+    use expectrl::spawn;
+
+    #[test]
+    fn test() {
+        let mut p = spawn("docker run --rm -it kalilinux/kali-rolling:latest echo hello").expect("failed to spawn");
+        let m = p.expect("hello").expect("no hello there");
+        assert_eq!(m.get(0).unwrap(), b"hello");
+    }
+}
